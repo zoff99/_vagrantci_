976e21efe815ba972eeea247b848acd318cb4c31
Zoff <zoff99@users.noreply.github.com>
2016-08-12 14:21:04 +0200

    add

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -2,7 +2,17 @@
 
 echo "configuring CI machine..."
 
-apt-get update
+
+# circleCI specific ---
+groupadd -g 1000 ubuntu
+useradd -m -u 1000 -g 1000 -s /bin/bash -d /home/ubuntu ubuntu"
+echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
+chown ubuntu:ubuntu /home/ubuntu"
+
+apt-get update --fix-missing"
+apt-get install -q -y g++ make git curl vim htop bc"
+
+
 apt-get install -y apache2
 
 if ! [ -L /var/www ]; then
