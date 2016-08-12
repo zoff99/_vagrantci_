5802a2490607b738d6e90f8a87c0975c8237ae57
Zoff <zoff99@users.noreply.github.com>
2016-08-12 16:35:38 +0200

    fix errors

--- tools/bootstrap.sh
+++ tools/bootstrap.sh
@@ -4,15 +4,16 @@ echo "configuring CI machine..."
 
 
 # circleCI specific ---
-groupadd -g 1000 ubuntu
-useradd -m -u 1000 -g 1000 -s /bin/bash -d /home/ubuntu ubuntu"
-echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"
-chown ubuntu:ubuntu /home/ubuntu"
+groupadd -g 99000 ubuntu
+useradd -m -u 1000 -g 99000 -s /bin/bash -d /home/ubuntu ubuntu
+echo 'ubuntu ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers
+chown ubuntu:ubuntu /home/ubuntu
 
-apt-get update --fix-missing"
-apt-get install -q -y g++ make git curl vim htop bc"
+apt-get update --fix-missing
+apt-get install -q -y g++ make git curl vim htop bc
 
 # install packages
+cat tools/circle_pkgs.txt
 cat tools/circle_pkgs.txt | xargs -L10 apt-get install
 
 apt-get install -y apache2
