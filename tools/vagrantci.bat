92899f7301c6e6e59c17503ff0a6b31ca7efc365
Zoff <zoff99@users.noreply.github.com>
2016-08-20 14:43:45 +0200

    add crude script for windows

---
+++ tools/vagrantci.bat
@@ -0,0 +1,29 @@
+REM
+REM VagrantCI - a Poor Man's CI System
+REM Copyright (C) 2016  Zoff <zoff@zoff.cc>
+REM
+REM This program is free software; you can redistribute it and/or
+REM modify it under the terms of the GNU General Public License
+REM as published by the Free Software Foundation; either version 2
+REM of the License, or (at your option) any later version.
+REM
+REM This program is distributed in the hope that it will be useful,
+REM but WITHOUT ANY WARRANTY; without even the implied warranty of
+REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+REM GNU General Public License for more details.
+REM
+REM You should have received a copy of the GNU General Public License
+REM along with this program; if not, write to the Free Software
+REM Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+REM
+
+REM C:\HashiCorp\Vagrant\bin\vagrant
+
+
+REM -- this is very basic !! --
+REM -- this is very basic !! --
+vagrant destroy --force
+vagrant up --provision
+REM -- this is very basic !! --
+REM -- this is very basic !! --
+
