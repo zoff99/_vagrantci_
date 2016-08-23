8a2aad7ed70abb1bef744a059af84947329d1ea3
Zoff <zoff99@users.noreply.github.com>
2016-08-23 21:33:45 +0200

    Create svn__update_and_build.sh

---
+++ svn__update_and_build.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+new=`cd .. && svn info -r HEAD 2>/dev/null | grep -i "Last Changed Rev" 2>/dev/null | cut -d':' -f2 | tr -d " "`
+old=`cd .. && svn info 2>/dev/null | grep -i "Last Changed Rev" 2>/dev/null | cut -d':' -f2 | tr -d " "`
+
+echo "old rev=""$old"
+echo "new rev=""$new"
+
+if [ "$old""x" != "$new""x" ]; then
+        echo ""
+        echo "============================="
+        echo "update and run..."
+        echo "============================="
+        echo ""
+        svn up .. && bash vagrantci.sh
+else
+        echo ""
+        echo "============================="
+        echo "no update"
+        echo "============================="
+        echo ""
+fi
