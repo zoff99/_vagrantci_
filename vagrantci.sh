517d0f626e39d494cc2de375b9e28b0c76199bc9
Zoff <zoff99@users.noreply.github.com>
2016-08-26 21:04:02 +0200

    handle errors better

--- vagrantci.sh
+++ vagrantci.sh
@@ -128,10 +128,32 @@ else
                                 echo " ** resetting to VM snapshot vagrantci001 ** "
                                 echo " ** CI run ** "
                                 vagrant snapshot restore "vagrantci001" </dev/null
+                                res1=$?
 
-                                # echo "$vagrantci__buf"
-                                # echo ""
-                                # vagrant up --provision </dev/null
+                                if [ $res1 -ne 0 ]; then
+                                        echo ""
+                                        echo " ++ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "
+                                        echo " ++ "
+                                        echo " ++     ERROR "
+                                        echo " ++ "
+                                        echo " ++ -- retrying -- "
+                                        echo " ++ "
+                                        echo " ++ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! "
+
+                                        echo "$vagrantci__buf"
+                                        echo " ** wait a bit ** "
+                                        sleep 120
+
+                                        echo "$vagrantci__header"
+                                        echo " ** halting VM ** "
+                                        vagrant suspend </dev/null
+                                        vagrant halt --force </dev/null
+
+                                        echo "$vagrantci__buf"
+                                        echo " ** resetting to VM snapshot vagrantci001 ** "
+                                        echo " ** CI run ** "
+                                        vagrant snapshot restore "vagrantci001" </dev/null
+                                fi
 
                                 echo "$vagrantci__footer"
                         fi
