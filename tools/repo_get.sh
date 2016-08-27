ef4b832be28907f967d6ab8026c442f7b061747c
Zoff <zoff99@users.noreply.github.com>
2016-08-27 22:11:30 +0200

    fix

--- tools/repo_get.sh
+++ tools/repo_get.sh
@@ -296,10 +296,10 @@ if [ $level_0_keys > 0 ]; then
 						echo "$_key"':'"$ci_cache_datadir"'/'"$_cache_data_file"'.tar' >> "$ci_cache_dirs"
 
 						b_key=`basename "$_key"`
-						echo 'cd '"$_key"'/../ ; tar -cvf '"$_cache_data_file"' '"$b_key" >> "$bdir"/dependencies/cache_directories/0_new_dirs.txt
+						echo 'cd '"$_key"'/../ && tar -cvf '"$_cache_data_file"' '"$b_key" >> "$bdir"/dependencies/cache_directories/0_new_dirs.txt
 					fi
 
-					echo 'cd '"$_key"'/../ ; tar -xvf '"$_cache_data_file" >> "$bdir"/dependencies/cache_directories/1_all_dirs.txt
+					echo 'mkdir -p '"$_key"' && cd '"$_key"'/../ && tar -xvf '"$_cache_data_file" >> "$bdir"/dependencies/cache_directories/1_all_dirs.txt
 
 				done
 
