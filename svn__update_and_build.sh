1503df3107e5d405829857eb962d5df78d47bd86
Zoff <zoff99@users.noreply.github.com>
2016-09-11 10:44:06 +0200

    less output

--- svn__update_and_build.sh
+++ svn__update_and_build.sh
@@ -3,8 +3,8 @@
 new=`cd .. && svn info -r HEAD 2>/dev/null | grep -i "Last Changed Rev" 2>/dev/null | cut -d':' -f2 | tr -d " "`
 old=`cd .. && svn info 2>/dev/null | grep -i "Last Changed Rev" 2>/dev/null | cut -d':' -f2 | tr -d " "`
 
-echo "old rev=""$old"
-echo "new rev=""$new"
+# echo "old rev=""$old"
+# echo "new rev=""$new"
 
 if [ "$old""x" != "$new""x" ]; then
 	echo ""
@@ -14,9 +14,10 @@ if [ "$old""x" != "$new""x" ]; then
 	echo ""
 	svn up .. && bash vagrantci.sh run
 else
-	echo ""
-	echo "============================="
-	echo "no update"
-	echo "============================="
-	echo ""
+	:
+	# echo ""
+	# echo "============================="
+	# echo "no update"
+	# echo "============================="
+	# echo ""
 fi
